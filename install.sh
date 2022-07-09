6d520091406fd5032d2f9c02e742398504851ba2
Epoch3kon <100540061+Epoch3kon@users.noreply.github.com>
2022-07-08 17:16:15 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,6 +5,8 @@ sudo pacman -Sy python-pywal i3-gaps xorg xorg-server xorg-xinit feh
 echo "Copying Config files"
 cp .Xresources ~/.Xresources
 cp config ~/.config/i3/config
+mkdir config/
+mkdir config/i3
 sudo cp i3blocks.conf /etc/i3blocks.conf
 mkdir Pictures
 cp background.png ~/Pictures/
